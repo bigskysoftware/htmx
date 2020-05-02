f801ca64457cb2d652a08a664f4974528ca65a0c
carson <carson@leaddyno.com>
2020-05-02 14:33:11 -0700

    bump

--- src/htmx.js
+++ src/htmx.js
@@ -898,7 +898,7 @@ var HTMx = HTMx || (function () {
         return {
             processElement: processNode,
             on: addHTMxEventListener,
-            version: "0.0.1",
+            version: "0.0.2",
             _:internalEval
         }
     }
