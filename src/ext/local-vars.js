d930d567aeb8e7ed05a7b6d0a76a57964fee2b4f
cscortes <cscortes@users.noreply.github.com>
2020-06-24 14:52:45 -0600

    Added symmetric HX-Local-Vars headers on both request and response.  Makes the backend programming more logical.  Also avoids clashes on session-vars.

--- src/ext/local-vars.js
+++ src/ext/local-vars.js
@@ -1,22 +1,29 @@
 htmx.defineExtension('local-vars', {
     onEvent: function (name, evt) {
+        if (!localStorage) {
+            console.log("Warning: browser does not support local storage.")
+        }
+        elt = evt.detail.elt;
         if (name === "configRequest.htmx") {
-            var includeLocalVarsElt = htmx.closest(evt.detail.elt, "[include-local-vars],[data-include-local-vars]");
-            if (includeLocalVarsElt) {
-                var includeVars = includeLocalVarsElt.getAttribute("include-local-vars") || includeLocalVarsElt.getAttribute("data-include-local-vars");
+            if (elt.hasAttribute("hx-include-local-vars") || elt.hasAttribute("data-hx-include-local-vars") ) {
+                var includeVars = elt.getAttribute("hx-include-local-vars") || elt.getAttribute("data-hx-include-local-vars");
+                var data = {};
                 includeVars.split(" ").forEach(function(key) {
                     var item = localStorage.getItem(key);
                     if(item) {
-                        evt.detail.parameters[key] = item;
+                        data[key] = item;
                     }
-                });                    
+                });
+                evt.detail.headers["HX-Local-Vars"] = JSON.stringify(data);           
+            }
+            else {
+                console.log("Warning: hx-local-vars without hx-include-local-vars.")
             }
         }
     },
     transformResponse: function(text, xhr, elt) {
-        var includeLocalVarsElt = htmx.closest(elt, "[include-local-vars],[data-include-local-vars]");
-        if (includeLocalVarsElt) {
-            var localVars = xhr.getResponseHeader("HX-Set-Local-Vars");
+        if (elt.hasAttribute("hx-include-local-vars") || elt.hasAttribute("data-hx-include-local-vars")) {
+            var localVars = xhr.getResponseHeader("HX-Local-Vars");
             if (localVars) {
                 var localVars = JSON.parse(localVars);
                 for(key in localVars) {
