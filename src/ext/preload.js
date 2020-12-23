7eb8e3fb90e5afa87b25e016181323970c4ba897
Ben Pate <ben@pate.org>
2020-12-23 13:46:02 -0700

    Configuraion Enhancements
    This extension now allows a configuration argument `preload="event,event,event"` on any node in the DOM. This argument can be inherited from parent nodes, and defaults to "mouseover,touchstart"

--- src/ext/preload.js
+++ src/ext/preload.js
@@ -1,23 +1,44 @@
 htmx.defineExtension("preload", {
-    onEvent: function(name, event) {
-        if (name == "htmx:processedNode") {
-            event.target.querySelectorAll("[preload]").forEach(function(node) {
-                if (node.preload == undefined) {
-                    var target = node.getAttribute("href") || node.getAttribute("hx-get")
-                    if (target != undefined) {
-                        node.addEventListener("mouseover", function() {
 
-                            if (node.preloaded == undefined) {
-                                var r = new XMLHttpRequest();
-                                r.open("GET", target);
-                                r.send();
-                                node.preloaded = true;
-                            }
-                        })
-                        node.preload = true;
-                    }
-                }
-            })
-        }
-    }
+	onEvent: function(name, event) {
+
+		if (name == "htmx:processedNode") {
+
+			event.target.querySelectorAll("[preload]").forEach(function(node) {
+
+				if (node.preloadState == undefined) {
+					node.preloadState = false;
+
+					var closest = function(node,attr) {
+						if (node == undefined) {return undefined}
+						return node.getAttribute(attr) || closest(node.parent)
+					}
+			
+					var preload = function(node) {
+						if (node.preloadState !== true) {
+							var target = node.getAttribute("href") || node.getAttribute("hx-get")
+							if (target != undefined) {
+								var r = new XMLHttpRequest();
+								r.open("GET", target);
+								r.send();
+								node.preloadState = true;
+							}
+						}
+					}
+
+					var events = (closest(node, "preload") || "mouseover,touchstart").split(",")
+
+					events.forEach(function(event) {
+						if (event === "load") {
+							preload(node)
+							return
+						}                        
+						node.addEventListener(event, function() {
+							preload(node)
+						})
+					})
+				}
+			})
+		}
+	}
 })
