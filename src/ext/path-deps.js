19b436773dd66bfb2e83ef60e355a15e219b9d7d
Johan Vosloo <jvosloo@gmail.com>
2020-12-26 22:58:17 +0100

    Update path-deps.js
    By catering for a new custom event called `htmx:refreshPath`, we should be able to support the triggering of the refresh of a specific path

--- src/ext/path-deps.js
+++ src/ext/path-deps.js
@@ -15,20 +15,26 @@
         return false;
     }
 
+    function refreshPath(path) {
+        var eltsWithDeps = htmx.findAll("[path-deps]");
+        for (var i = 0; i < eltsWithDeps.length; i++) {
+            var elt = eltsWithDeps[i];
+            if (dependsOn(elt.getAttribute('path-deps'), path)) {
+                htmx.trigger(elt, "path-deps");
+            }
+        }      
+    }
+
     htmx.defineExtension('path-deps', {
         onEvent: function (name, evt) {
             if (name === "htmx:afterRequest") {
                 var config = evt.detail.requestConfig;
                 // mutating call
                 if (config.verb !== "get") {
-                    var eltsWithDeps = htmx.findAll("[path-deps]");
-                    for (var i = 0; i < eltsWithDeps.length; i++) {
-                        var elt = eltsWithDeps[i];
-                        if (dependsOn(elt.getAttribute('path-deps'), config.path)) {
-                            htmx.trigger(elt, "path-deps");
-                        }
-                    }
+                    refreshPath(config.path);
                 }
+            } else if (name === "htmx:refreshPath") {
+                refreshPath(evt.path);
             }
         }
     });
