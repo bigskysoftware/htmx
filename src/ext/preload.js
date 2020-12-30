294885454ba600d009622b1c67455f600ba7ec5f
Ben Pate <ben@pate.org>
2020-12-29 20:39:07 -0700

    Style updates
    - use built-in htmx.parseInterval function instead of custom one
    - smarter handling of mouse movement inside complex objects
    - more descriptive values in preloadState
    - also, semicolons

--- src/ext/preload.js
+++ src/ext/preload.js
@@ -8,7 +8,7 @@ htmx.defineExtension("preload", {
 
 		// Only take actions on "htmx:processedNode"
 		if (name !== "htmx:processedNode") {
-			return
+			return;
 		}
 
 		// SOME HELPER FUNCTIONS WE'LL NEED ALONG THE WAY
@@ -18,13 +18,13 @@ htmx.defineExtension("preload", {
 		// would return "100ms".
 		var closest = function(node, token) {
 			// Handle undefined inputs gracefully
-			if (node == undefined) {return undefined}
+			if (node == undefined) {return undefined;}
 
 			// Get the attribute
-			var attr = node.getAttribute("preload")
+			var attr = node.getAttribute("preload");
 
 			// If we find a token in this attribute value, return it.  Otherwise, search parent elements.
-			return parseToken(attr, token) || closest(node.parentElement, token)
+			return parseToken(attr, token) || closest(node.parentElement, token);
 		}
 
 		// parseToken finds the value for a specific name:value pair
@@ -34,24 +34,24 @@ htmx.defineExtension("preload", {
 
 			// Handle undefined inputs gracefully
 			if (input == undefined) {
-				return undefined
+				return undefined;
 			}
 
 			// Split options on whitespace
-			var options = input.split(/\s/)
+			var options = input.split(/\s/);
 
 			// Search all options for a matching name...
 			for (var i = 0 ; i < options.length ; i++) {
-				var option = options[i].split(":")
+				var option = options[i].split(":");
 				if (option[0] === name) {
-					return option[1] // ... return token value
+					return option[1]; // ... return token value
 				}
 			}
 
 			// Nothing found, return undefined
-			return undefined
+			return undefined;
 		}
-
+		
 		// load handles the actual HTTP fetch, and uses htmx.ajax in cases where we're 
 		// preloading an htmx resource (this sends the same HTTP headers as a regular htmx request)
 		var load = function(node) {
@@ -59,8 +59,8 @@ htmx.defineExtension("preload", {
 			return function() {
 
 				// If this value has already been loaded, then do not try again.
-				if (node.preloadState === true) {
-					return
+				if (node.preloadState !== "READY") {
+					return;
 				}
 
 				// Special handling for HX_GET - use built-in htmx.ajax function
@@ -68,10 +68,10 @@ htmx.defineExtension("preload", {
 				// node.preloadState = TRUE so that requests are not duplicated
 				// in the future
 				if (node.getAttribute("hx-get")) {
-					htmx.ajax("GET", node.getAttribute("hx-get"), {handler:function(){
-						node.preloadState = true;
-					}})
-					return
+					htmx.ajax("GET", node.getAttribute("hx-get"), {handler:function() {
+						node.preloadState = "DONE"
+					}});
+					return;
 				}
 
 				// Otherwise, perform a standard xhr request, then set 
@@ -81,35 +81,11 @@ htmx.defineExtension("preload", {
 					var r = new XMLHttpRequest();
 					r.open("GET", node.getAttribute("href"));
 					r.onload = function() {
-						node.preloadState = true;
+						node.preloadState = "DONE";
 					}
 					r.send();
 				}
 			}
-		}
-
-
-		// parseTime parses a time string into milliseconds.  It understands
-		// seconds and milliseconds.  Unlabeled values are assumed to be milliseconds.
-		//
-		// parseTime("100ms") => 100
-		// parseTime("10s") => 10,000
-		// parseTime("1") => 1
-		var parseTime = function(value) {
-
-			if (value == undefined)  {
-				return undefined
-			}
-
-			if (value.slice(-2) == "ms") {
-				return parseInt(value.slice(0,-2)) || undefined
-			}
-			
-			if (value.slice(-1) == "s") {
-				return (parseInt(value.slice(0,-1)) * 1000) || undefined
-			}
-
-			return parseInt(value) || undefined
 		}
 
 		// Search for all child nodes that have a "preload" attribute.  Making this explicit
@@ -118,52 +94,55 @@ htmx.defineExtension("preload", {
 
 			// Guarantee that we only process each node once.
 			if (node.preloadState !== undefined) {
-				return
+				return;
 			}
 			
 			// This means that the node has been initialized
-			node.preloadState = "INIT"
+			node.preloadState = "PAUSE";
 
 			// Get event name.  Default="mousedown"
-			var on = closest(node, "on") || "mousedown"
+			var on = closest(node, "on") || "mousedown";
 			
 			// One-Line monstrosity to get wait time.  For mouseover events, Default=100ms.  All others, default=0ms.
-			var wait = parseTime(closest(node, "wait")) || (on == "mouseover") ? 100 : 0
-			
+			var wait = htmx.parseInterval(closest(node, "wait")) || ((on == "mouseover") ? 100 : 0);
+
 			// Special handling for "load" events.  No EventListener necessary, just trigger the timer now.
 			if (on === "load") {
-				node.preloadState = "WAITING"  // Required for the `load` function to trigger
-				window.setTimeout(load(node), wait) // Do the thing...
-				return
+				node.preloadState = "READY";  // Required for the `load` function to trigger
+				window.setTimeout(load(node), wait);
+				return;
 			}
 			
 			// FALL THROUGH to here means we need to add an EventListener
-
+	
 			// Apply the listener to the node
 			node.addEventListener(on, function() {
-				node.preloadState = "WAITING" // Requred for the `load` function to trigger
-				window.setTimeout(load(node), wait) // Do the thing...
+				// Only add one event listener
+				if (node.preloadState !== "READY") {
+					node.preloadState = "READY"; // Requred for the `load` function to trigger
+					window.setTimeout(load(node), wait);
+				}
 			})
 
 			switch (on) {
 
 				// Special handling for "mouseover" events
 				case "mouseover":
-					// Mirror `touchstart` events, too
-					node.addEventListener("touchstart", load(node))
+					// Mirror `touchstart` events (fires immediately)
+					node.addEventListener("touchstart", load(node));
 
-					// Check to see if the mouse leaves, too...
-					node.addEventListener("mouseout", function() {
-						if (node.preloadState == "WAITING") { // ...if we're still waiting...
-							node.preloadState = "INIT" // ... then don't wait anymore, skip this preload event.
+					// WHhen the mouse leaves, immediately disable the preload
+					node.addEventListener("mouseout", function(evt) {
+						if (evt.target === node) {
+							node.preloadState = "PAUSE"
 						}
 					})
 					break;
 
 				// Special handling for "mousedown" events
 				case "mousedown":
-					 // Mirror `touchstart` events, too
-					node.addEventListener("touchstart", load(node))
+					 // Mirror `touchstart` events (fires immediately)
+					node.addEventListener("touchstart", load(node));
 					break;
 			}
 		})
