1e780a7961607f60d0fd10d0d665dafbb9d94d45
carson <carson@leaddyno.com>
2021-01-03 15:59:01 -0700

    include `preload` in the website

--- scripts/www.js
+++ scripts/www.js
@@ -14,6 +14,7 @@ fs.copySync("test/", currentReleaseRoot + "/test");
 fs.copySync("src/", currentReleaseRoot + "/src");
 fs.copySync("src/htmx.js", "www/js/htmx.js");
 fs.copySync("src/ext/class-tools.js", "www/js/class-tools.js");
+fs.copySync("src/ext/preload.js", "www/js/preload.js");
 
 var testHTML = "<html><body style='font-family: sans-serif'><h1>HTMX TESTS</h1><ul>\n"
 fs.readdirSync(testRoot).reverse().forEach(function(file){
