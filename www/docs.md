25b5aa337f957cf61033c72871299f7ef6eac191
carson <carson@leaddyno.com>
2021-04-05 07:45:51 -0600

    release prep

--- www/docs.md
+++ www/docs.md
@@ -101,7 +101,7 @@ It can be used via [NPM](https://www.npmjs.com/) as "`htmx.org`" or downloaded o
 [unpkg](https://unpkg.com/browse/htmx.org/) or your other favorite NPM-based CDN:
 
 ``` html
-    <script src="https://unpkg.com/htmx.org@1.3.2"></script>
+    <script src="https://unpkg.com/htmx.org@1.3.3"></script>
 ```
 
 For added security, you can load the script using [Subresource Integrity (SRI)](https://developer.mozilla.org/en-US/docs/Web/Security/Subresource_Integrity).
--- www/test/1.3.3/src/htmx.js
+++ www/test/1.3.3/src/htmx.js
@@ -49,7 +49,8 @@ return (function () {
                 swappingClass:'htmx-swapping',
                 allowEval:true,
                 attributesToSettle:["class", "style", "width", "height"],
-                wsReconnectDelay: 'full-jitter'
+                wsReconnectDelay: 'full-jitter',
+                disableSelector: "[hx-disable], [data-hx-disable]",
             },
             parseInterval:parseInterval,
             _:internalEval,
@@ -1355,6 +1356,9 @@ return (function () {
         }
 
         function initNode(elt) {
+            if (elt.closest && elt.closest(htmx.config.disableSelector)) {
+                return;
+            }
             var nodeData = getInternalData(elt);
             if (!nodeData.initialized) {
                 nodeData.initialized = true;
