87bbb1206738d674601aa22e98cd0aeb0a89c0c0
Telroshan <vichenzo-thebaud@hotmail.com>
2021-08-30 14:47:51 +0200

    Don't hide request indicators if response has a redirect header

--- src/htmx.js
+++ src/htmx.js
@@ -2379,7 +2379,11 @@ return (function () {
                 try {
                     var hierarchy = hierarchyForElt(elt);
                     responseHandler(elt, responseInfo);
-                    removeRequestIndicatorClasses(indicators);
+
+                    if (!hasHeader(xhr, /HX-Redirect:/i)) {
+                        removeRequestIndicatorClasses(indicators);
+                    }
+                    
                     triggerEvent(elt, 'htmx:afterRequest', responseInfo);
                     triggerEvent(elt, 'htmx:afterOnLoad', responseInfo);
                     // if the body no longer contains the element, trigger the even on the closest parent
