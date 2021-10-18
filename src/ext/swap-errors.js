1ced8942247c6f7435f615c00a589cc45889cbf1
Jimmy Girardet <ijkl@netc.fr>
2021-10-18 18:38:31 +0200

    add test

--- src/ext/swap-errors.js
+++ src/ext/swap-errors.js
@@ -1,9 +1,10 @@
-htmx.defineExtension('swap-errors', {
-    onEvent: function (name, evt) {
-        if (name === "htmx:beforeSwap") {
-            if ("HX-SWAP-ERRORS" in evt.detail.headers) {
-                evt.detail.shouldSwap=true
-            }
-        }
+htmx.defineExtension("swap-errors", {
+  onEvent: function (name, evt) {
+    if (name === "htmx:beforeSwap") {
+      if (evt.detail.xhr.getResponseHeader("HX-SWAP-ERRORS") !== null) {
+        evt.detail.shouldSwap = true;
+      }
     }
+  },
 });
+
--- test/ext/swap-errors.js
+++ test/ext/swap-errors.js
@@ -1,21 +1,22 @@
-describe("swap-error extension", function() {
-    beforeEach(function () {
-        this.server = makeServer();
-        clearWorkArea();
-    });
-    afterEach(function () {
-        this.server.restore();
-        clearWorkArea();
-    });
+describe("swap-error extension", function () {
+  beforeEach(function () {
+    this.server = makeServer();
+    clearWorkArea();
+  });
+  afterEach(function () {
+    this.server.restore();
+    clearWorkArea();
+  });
 
-    it('Sends the X-Requested-With header', function () {
-        this.server.respondWith("GET", "/test", function (xhr) {
-            xhr.respond(400, {}, xhr.responseHeaders['HX-SWAP-ERRORS'])
-        });
-        var btn = make('<button hx-get="/test" hx-ext="swap-errors">Click Me!</button>')
-        btn.click();
-        this.server.respond();
-        btn.innerHTML.should.equal("XMLHttpRequest");
+  it(" test les erreursr", function () {
+    this.server.respondWith("GET", "/test", function (xhr) {
+      xhr.respond(400, { "HX-SWAP-ERRORS": 1 }, "SWAPPED");
     });
-
+    var btn = make(
+      '<button hx-get="/test" hx-ext="swap-errors">Click Me!</button>'
+    );
+    btn.click();
+    this.server.respond();
+    btn.innerHTML.should.equal("SWAPPED");
+  });
 });
