ac38a1a7359ef03625bea33364a43c00741c3bd1
Carson Gross <carson@bigsky.software>
2022-10-02 13:56:41 -0600

    ignore empty attributes when computing attribute hash

--- src/htmx.js
+++ src/htmx.js
@@ -832,10 +832,12 @@ return (function () {
 
         function attributeHash(elt) {
             var hash = 0;
-            for (let i = 0; i < elt.attributes.length; i++) {
-                const attribute = elt.attributes[i];
-                hash = stringHash(attribute.name, hash);
-                hash = stringHash(attribute.value, hash);
+            for (var i = 0; i < elt.attributes.length; i++) {
+                var attribute = elt.attributes[i];
+                if(attribute.value){ // only include attributes w/ actual values (empty is same as non-existent)
+                    hash = stringHash(attribute.name, hash);
+                    hash = stringHash(attribute.value, hash);
+                }
             }
             return hash;
         }
