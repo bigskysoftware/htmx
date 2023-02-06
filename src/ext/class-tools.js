a75e2375af646c79e704b8906b2228cddcbb50c9
Epidiah Ravachol <75449595+Epidiah@users.noreply.github.com>
2023-02-06 17:44:36 -0500

    fix class-tools issue

--- src/ext/class-tools.js
+++ src/ext/class-tools.js
@@ -1,18 +1,18 @@
-(function(){
+(function () {
 
     function splitOnWhitespace(trigger) {
         return trigger.split(/\s+/);
     }
 
     function parseClassOperation(trimmedValue) {
-        var split = splitOnWhitespace(trimmedValue);
+        let split = splitOnWhitespace(trimmedValue);
         if (split.length > 1) {
-            var operation = split[0];
-            var classDef = split[1].trim();
-            var cssClass;
-            var delay;
+            let operation = split[0];
+            let classDef = split[1].trim();
+            let cssClass;
+            let delay;
             if (classDef.indexOf(":") > 0) {
-                var splitCssClass = classDef.split(':');
+                let splitCssClass = classDef.split(':');
                 cssClass = splitCssClass[0];
                 delay = htmx.parseInterval(splitCssClass[1]);
             } else {
@@ -20,38 +20,46 @@
                 delay = 100;
             }
             return {
-                operation:operation,
-                cssClass:cssClass,
-                delay:delay
+                operation: operation,
+                cssClass: cssClass,
+                delay: delay
             }
         } else {
             return null;
         }
     }
 
+    function performOperation(elt, classOperation, classList, currentRunTime) {
+        setTimeout(function () {
+            elt.classList[classOperation.operation].call(elt.classList, classOperation.cssClass);
+        }, currentRunTime)
+    }
+
+    function toggleOperation(elt, classOperation, classList, currentRunTime) {
+        setTimeout(function () {
+            setInterval(function () {
+                elt.classList[classOperation.operation].call(elt.classList, classOperation.cssClass);
+            }, classOperation.delay);
+        }, currentRunTime)
+    }
+
     function processClassList(elt, classList) {
-        var runs = classList.split("&");
-        for (var i = 0; i < runs.length; i++) {
-            var run = runs[i];
-            var currentRunTime = 0;
-            var classOperations = run.split(",");
-            for (var j = 0; j < classOperations.length; j++) {
-                var value = classOperations[j];
-                var trimmedValue = value.trim();
-                var classOperation = parseClassOperation(trimmedValue);
+        let runs = classList.split("&");
+        for (let i = 0; i < runs.length; i++) {
+            let run = runs[i];
+            let currentRunTime = 0;
+            let classOperations = run.split(",");
+            for (let j = 0; j < classOperations.length; j++) {
+                let value = classOperations[j];
+                let trimmedValue = value.trim();
+                let classOperation = parseClassOperation(trimmedValue);
                 if (classOperation) {
                     if (classOperation.operation === "toggle") {
-                        setTimeout(function () {
-                            setInterval(function () {
-                                elt.classList[classOperation.operation].call(elt.classList, classOperation.cssClass);
-                            }, classOperation.delay);
-                        }, currentRunTime);
+                        toggleOperation(elt, classOperation, classList, currentRunTime);
                         currentRunTime = currentRunTime + classOperation.delay;
                     } else {
                         currentRunTime = currentRunTime + classOperation.delay;
-                        setTimeout(function () {
-                            elt.classList[classOperation.operation].call(elt.classList, classOperation.cssClass);
-                        }, currentRunTime);
+                        performOperation(elt, classOperation, classList, currentRunTime);
                     }
                 }
             }
@@ -60,7 +68,7 @@
 
     function maybeProcessClasses(elt) {
         if (elt.getAttribute) {
-            var classList = elt.getAttribute("classes") || elt.getAttribute("data-classes");
+            let classList = elt.getAttribute("classes") || elt.getAttribute("data-classes");
             if (classList) {
                 processClassList(elt, classList);
             }
@@ -70,11 +78,11 @@
     htmx.defineExtension('class-tools', {
         onEvent: function (name, evt) {
             if (name === "htmx:afterProcessNode") {
-                var elt = evt.detail.elt;
+                let elt = evt.detail.elt;
                 maybeProcessClasses(elt);
                 if (elt.querySelectorAll) {
-                    var children = elt.querySelectorAll("[classes], [data-classes]");
-                    for (var i = 0; i < children.length; i++) {
+                    let children = elt.querySelectorAll("[classes], [data-classes]");
+                    for (let i = 0; i < children.length; i++) {
                         maybeProcessClasses(children[i]);
                     }
                 }
