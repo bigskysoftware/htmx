f690d928342b6334fa539cfbd4be515748d2ba0f
Daniel J. Summers <daniel@bitbadger.solutions>
2023-03-18 11:29:38 -0400

    Replace www directory

--- www-zola/static/test/0.0.4/src/ext/json-enc.js
+++
@@ -1,7 +0,0 @@
-htmx.defineExtension('json-enc', {
-    encodeParameters : function(xhr, parameters, elt) {
-        xhr.requestHeaders['Content-Type'] = 'application/json';
-        xhr.overrideMimeType('text/json');
-        return (JSON.stringify(parameters));
-    }
-});
