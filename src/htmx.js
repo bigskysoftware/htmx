4246b8546eb0703154281cebfe913c5e354d7ad6
Vincent <vichenzo-thebaud@hotmail.com>
2023-06-23 23:30:45 +0200

    Encode as URL params for DELETE requests, instead of using body

--- src/htmx.js
+++ src/htmx.js
@@ -3001,7 +3001,7 @@ return (function () {
             var pathNoAnchor = splitPath[0];
             var anchor = splitPath[1];
             var finalPathForGet = null;
-            if (verb === 'get') {
+            if (verb === 'get' || verb === 'delete') {
                 finalPathForGet = pathNoAnchor;
                 var values = Object.keys(filteredParameters).length !== 0;
                 if (values) {
@@ -3015,7 +3015,7 @@ return (function () {
                         finalPathForGet += "#" + anchor;
                     }
                 }
-                xhr.open('GET', finalPathForGet, true);
+                xhr.open(verb.toUpperCase(), finalPathForGet, true);
             } else {
                 xhr.open(verb.toUpperCase(), path, true);
             }
