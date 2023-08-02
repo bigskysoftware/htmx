ed6da38ae1052da2d48b12f08c4902d0d1598053
Dave Syer <david_syer@hotmail.com>
2023-08-02 09:15:41 +0000

    Add interactive check to ready event handler
    If HTMX is imported in a module the readyState is "interactive"
    so the extension processing happens too late. This would fix it.

--- src/htmx.js
+++ src/htmx.js
@@ -3584,7 +3584,7 @@ return (function () {
         //====================================================================
 
         function ready(fn) {
-            if (getDocument().readyState !== 'loading') {
+            if (getDocument().readyState === 'complete') {
                 fn();
             } else {
                 getDocument().addEventListener('DOMContentLoaded', fn);
