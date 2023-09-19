b7601a2cebfe2bb372ad4767c47b44435ce7f466
Rouak-stha <rounakshrestha1153@gmail.com>
2023-09-19 22:30:10 +0545

    #1788 Fix URL Params Appending on Boost

--- src/htmx.js
+++ src/htmx.js
@@ -3102,7 +3102,7 @@ return (function () {
 
             // behavior of anchors w/ empty href is to use the current URL
             if (path == null || path === "") {
-                path = getDocument().location.href;
+                path = getDocument().location.href.split('?')[0];
             }
 
 
