d1288d202aecbe46417e6978615e21ba3c74c8cf
Alexander Petros <apetros15@gmail.com>
2023-09-19 12:07:24 -0400

    Remove old tests from the website (#1733)
    The website used to host every past test suite, copied into the www
    directory. We no longer need that on the website (and it makes the
    codebase impossible to search) so I removed all the old tests and the
    new tests are hosted simply at /test.
    
    I also replaced the www.js script with a simpler www.sh one (since we no
    longer need to do anything besides copying, really), which allowed me to
    remove a node dependency that was only used in that script.

---
+++ scripts/www.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -euo pipefail
+
+STATIC_ROOT="www/static"
+PACKAGE_VERSION=$(cat package.json | grep version | cut -d '"' -f 4)
+
+
+cp node_modules/mocha/mocha.js "$STATIC_ROOT/node_modules/mocha/mocha.js"
+cp node_modules/mocha/mocha.css "$STATIC_ROOT/node_modules/mocha/mocha.css"
+cp node_modules/chai/chai.js "$STATIC_ROOT/node_modules/chai/chai.js"
+cp node_modules/chai-dom/chai-dom.js "$STATIC_ROOT/node_modules/chai-dom/chai-dom.js"
+cp node_modules/sinon/pkg/sinon.js "$STATIC_ROOT/node_modules/sinon/pkg/sinon.js"
+cp node_modules/mock-socket/dist/mock-socket.js "$STATIC_ROOT/node_modules/mock-socket/dist/mock-socket.js"
+
+rm -rf "$STATIC_ROOT/test" "$STATIC_ROOT/src"
+cp -r "./test" "$STATIC_ROOT/test"
+cp -r "./src" "$STATIC_ROOT/src"
