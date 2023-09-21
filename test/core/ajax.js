398b1ff4a25255a4815c45511a2d3d8f866853af
Vincent <vichenzo-thebaud@hotmail.com>
2023-09-21 08:11:33 +0200

    Add a test to ensure inputs external to form work properly

--- test/core/ajax.js
+++ test/core/ajax.js
@@ -1208,4 +1208,26 @@ describe("Core htmx AJAX Tests", function(){
         this.server.respond();
         values.should.deep.equal({t1: 'textValue', b1: ['inputValue', 'buttonValue']});
     })
+
+    it('properly handles inputs external to form', function () {
+        var values;
+        this.server.respondWith("Post", "/test", function (xhr) {
+            values = getParameters(xhr);
+            xhr.respond(204, {}, "");
+        });
+
+        make('<form id="externalForm" hx-post="/test">' +
+            '   <input type="hidden" name="b1" value="inputValue">' +
+            '</form>' +
+            '<input type="text" name="t1" value="textValue" form="externalForm">' +
+            '<select name="s1" form="externalForm">' +
+            '   <option value="someValue"></option>' +
+            '   <option value="selectValue" selected></option>' +
+            '</select>' +
+            '<button id="submit" form="externalForm" type="submit" name="b1" value="buttonValue">button</button>');
+
+        byId("submit").click();
+        this.server.respond();
+        values.should.deep.equal({t1: 'textValue', b1: ['inputValue', 'buttonValue'], s1: "selectValue"});
+    })
 })
