4b890ce72d98895ff1ec4e83278272c0e2c9bc7e
Alexander Petros <apetros15@gmail.com>
2023-12-21 13:22:10 -0500

    Run linter on the tests too

--- test/attributes/hx-boost.js
+++ test/attributes/hx-boost.js
@@ -1,118 +1,113 @@
-describe("hx-boost attribute", function() {
+describe('hx-boost attribute', function() {
+  beforeEach(function() {
+    this.server = makeServer()
+    clearWorkArea()
+  })
+  afterEach(function() {
+    this.server.restore()
+    clearWorkArea()
+  })
 
-    beforeEach(function () {
-        this.server = makeServer();
-        clearWorkArea();
-    });
-    afterEach(function () {
-        this.server.restore();
-        clearWorkArea();
-    });
+  it('handles basic anchor properly', function() {
+    this.server.respondWith('GET', '/test', 'Boosted')
+    var div = make('<div hx-target="this" hx-boost="true"><a id="a1" href="/test">Foo</a></div>')
+    var a = byId('a1')
+    a.click()
+    this.server.respond()
+    div.innerHTML.should.equal('Boosted')
+  })
 
-    it('handles basic anchor properly', function () {
-        this.server.respondWith("GET", "/test", "Boosted");
-        var div = make('<div hx-target="this" hx-boost="true"><a id="a1" href="/test">Foo</a></div>');
-        var a = byId('a1');
-        a.click();
-        this.server.respond();
-        div.innerHTML.should.equal("Boosted");
+  it('handles basic form post properly', function() {
+    this.server.respondWith('POST', '/test', 'Boosted')
+    var div = make('<div hx-target="this" hx-boost="true"><form id="f1" action="/test" method="post"><button id="b1">Submit</button></form></div>')
+    var btn = byId('b1')
+    btn.click()
+    this.server.respond()
+    div.innerHTML.should.equal('Boosted')
+  })
+
+  it('handles basic form post with button formaction properly', function() {
+    this.server.respondWith('POST', '/test', 'Boosted')
+    var div = make('<div hx-target="this" hx-boost="true"><form id="f1" method="post"><button id="b1" formaction="/test">Submit</button></form></div>')
+    var btn = byId('b1')
+    btn.click()
+    this.server.respond()
+    div.innerHTML.should.equal('Boosted')
+  })
+
+  it('handles basic form post with button formmethod properly', function() {
+    this.server.respondWith('POST', '/test', 'Boosted')
+    var div = make('<div hx-target="this" hx-boost="true"><form id="f1" action="/test"><button id="b1" formmethod="post">Submit</button></form></div>')
+    var btn = byId('b1')
+    btn.click()
+    this.server.respond()
+    div.innerHTML.should.equal('Boosted')
+  })
+
+  it('handles basic form post properly w/ explicit action', function() {
+    this.server.respondWith('POST', '/test', 'Boosted')
+    var div = make('<div hx-target="this"><form id="f1" action="/test" method="post"  hx-trigger="click" hx-boost="true"></form></div>')
+    var form = byId('f1')
+    form.click()
+    this.server.respond()
+    div.innerHTML.should.equal('Boosted')
+  })
+
+  it('handles basic form get properly', function() {
+    this.server.respondWith('GET', '/test', 'Boosted')
+    var div = make('<div hx-target="this" hx-boost="true"><form id="f1" action="/test" method="get"><button id="b1">Submit</button></form></div>')
+    var btn = byId('b1')
+    btn.click()
+    this.server.respond()
+    div.innerHTML.should.equal('Boosted')
+  })
+
+  it('handles basic form with no explicit method property', function() {
+    this.server.respondWith('GET', '/test', 'Boosted')
+    var div = make('<div hx-target="this" hx-boost="true"><form id="f1" action="/test"><button id="b1">Submit</button></form></div>')
+    var btn = byId('b1')
+    btn.click()
+    this.server.respond()
+    div.innerHTML.should.equal('Boosted')
+  })
+
+  it('handles basic anchor properly w/ data-* prefix', function() {
+    this.server.respondWith('GET', '/test', 'Boosted')
+    var div = make('<div data-hx-target="this" data-hx-boost="true"><a id="a1" href="/test">Foo</a></div>')
+    var a = byId('a1')
+    a.click()
+    this.server.respond()
+    div.innerHTML.should.equal('Boosted')
+  })
+
+  it('overriding default swap style does not effect boosting', function() {
+    htmx.config.defaultSwapStyle = 'afterend'
+    try {
+      this.server.respondWith('GET', '/test', 'Boosted')
+      var a = make('<a hx-target="this" hx-boost="true" id="a1" href="/test">Foo</a>')
+      a.click()
+      this.server.respond()
+      a.innerHTML.should.equal('Boosted')
+    } finally {
+      htmx.config.defaultSwapStyle = 'innerHTML'
+    }
+  })
+
+  it('anchors w/ explicit targets are not boosted', function() {
+    var a = make('<a hx-target="this" hx-boost="true" id="a1" href="/test" target="_blank">Foo</a>')
+    var internalData = htmx._('getInternalData')(a)
+    should.equal(undefined, internalData.boosted)
+  })
+
+  it('includes an HX-Boosted Header', function() {
+    this.server.respondWith('GET', '/test', function(xhr) {
+      should.equal(xhr.requestHeaders['HX-Boosted'], 'true')
+      xhr.respond(200, {}, 'Boosted!')
     })
 
-
-    it('handles basic form post properly', function () {
-        this.server.respondWith("POST", "/test", "Boosted");
-        var div = make('<div hx-target="this" hx-boost="true"><form id="f1" action="/test" method="post"><button id="b1">Submit</button></form></div>');
-        var btn = byId('b1');
-        btn.click();
-        this.server.respond();
-        div.innerHTML.should.equal("Boosted");
-    })
-
-    it('handles basic form post with button formaction properly', function () {
-        this.server.respondWith("POST", "/test", "Boosted");
-        var div = make('<div hx-target="this" hx-boost="true"><form id="f1" method="post"><button id="b1" formaction="/test">Submit</button></form></div>');
-        var btn = byId('b1');
-        btn.click();
-        this.server.respond();
-        div.innerHTML.should.equal("Boosted");
-    })
-
-    it('handles basic form post with button formmethod properly', function () {
-        this.server.respondWith("POST", "/test", "Boosted");
-        var div = make('<div hx-target="this" hx-boost="true"><form id="f1" action="/test"><button id="b1" formmethod="post">Submit</button></form></div>');
-        var btn = byId('b1');
-        btn.click();
-        this.server.respond();
-        div.innerHTML.should.equal("Boosted");
-    })
-
-    it('handles basic form post properly w/ explicit action', function () {
-        this.server.respondWith("POST", "/test", "Boosted");
-        var div = make('<div hx-target="this"><form id="f1" action="/test" method="post"  hx-trigger="click" hx-boost="true"></form></div>');
-        var form = byId('f1');
-        form.click();
-        this.server.respond();
-        div.innerHTML.should.equal("Boosted");
-    })
-
-    it('handles basic form get properly', function () {
-        this.server.respondWith("GET", "/test", "Boosted");
-        var div = make('<div hx-target="this" hx-boost="true"><form id="f1" action="/test" method="get"><button id="b1">Submit</button></form></div>');
-        var btn = byId('b1');
-        btn.click();
-        this.server.respond();
-        div.innerHTML.should.equal("Boosted");
-    })
-
-    it('handles basic form with no explicit method property', function () {
-        this.server.respondWith("GET", "/test", "Boosted");
-        var div = make('<div hx-target="this" hx-boost="true"><form id="f1" action="/test"><button id="b1">Submit</button></form></div>');
-        var btn = byId('b1');
-        btn.click();
-        this.server.respond();
-        div.innerHTML.should.equal("Boosted");
-    })
-
-    it('handles basic anchor properly w/ data-* prefix', function () {
-        this.server.respondWith("GET", "/test", "Boosted");
-        var div = make('<div data-hx-target="this" data-hx-boost="true"><a id="a1" href="/test">Foo</a></div>');
-        var a = byId('a1');
-        a.click();
-        this.server.respond();
-        div.innerHTML.should.equal("Boosted");
-    })
-
-
-    it('overriding default swap style does not effect boosting', function () {
-        htmx.config.defaultSwapStyle = "afterend";
-        try {
-            this.server.respondWith("GET", "/test", "Boosted");
-            var a = make('<a hx-target="this" hx-boost="true" id="a1" href="/test">Foo</a>');
-            a.click();
-            this.server.respond();
-            a.innerHTML.should.equal("Boosted");
-        } finally {
-            htmx.config.defaultSwapStyle = "innerHTML";
-        }
-    })
-
-    it('anchors w/ explicit targets are not boosted', function () {
-        var a = make('<a hx-target="this" hx-boost="true" id="a1" href="/test" target="_blank">Foo</a>');
-        var internalData = htmx._("getInternalData")(a);
-        should.equal(undefined, internalData.boosted);
-    })
-
-    it('includes an HX-Boosted Header', function()
-    {
-        this.server.respondWith("GET", "/test", function(xhr){
-            should.equal(xhr.requestHeaders['HX-Boosted'], "true");
-            xhr.respond(200, {}, "Boosted!");
-        });
-
-        var btn = make('<a hx-boost="true" hx-target="this" href="/test">Click Me!</a>')
-        btn.click();
-        this.server.respond();
-        btn.innerHTML.should.equal("Boosted!");
-    });
-
-});
+    var btn = make('<a hx-boost="true" hx-target="this" href="/test">Click Me!</a>')
+    btn.click()
+    this.server.respond()
+    btn.innerHTML.should.equal('Boosted!')
+  })
+})
--- test/attributes/hx-select.js
+++ test/attributes/hx-select.js
@@ -1,40 +1,37 @@
-describe("BOOTSTRAP - htmx AJAX Tests", function(){
-    beforeEach(function() {
-        this.server = makeServer();
-        clearWorkArea();
-    });
-    afterEach(function()  {
-        this.server.restore();
-        clearWorkArea();
-    });
+describe('BOOTSTRAP - htmx AJAX Tests', function() {
+  beforeEach(function() {
+    this.server = makeServer()
+    clearWorkArea()
+  })
+  afterEach(function() {
+    this.server.restore()
+    clearWorkArea()
+  })
 
-    it('properly handles a partial of HTML', function()
-    {
-        var i = 1;
-        this.server.respondWith("GET", "/test", "<div id='d1'>foo</div><div id='d2'>bar</div>");
-        var div = make('<div hx-get="/test" hx-select="#d1"></div>');
-        div.click();
-        this.server.respond();
-        div.innerHTML.should.equal("<div id=\"d1\">foo</div>");
-    });
+  it('properly handles a partial of HTML', function() {
+    var i = 1
+    this.server.respondWith('GET', '/test', "<div id='d1'>foo</div><div id='d2'>bar</div>")
+    var div = make('<div hx-get="/test" hx-select="#d1"></div>')
+    div.click()
+    this.server.respond()
+    div.innerHTML.should.equal('<div id="d1">foo</div>')
+  })
 
-    it('properly handles a full HTML document', function()
-    {
-        var i = 1;
-        this.server.respondWith("GET", "/test", "<html><body><div id='d1'>foo</div><div id='d2'>bar</div></body></html>");
-        var div = make('<div hx-get="/test" hx-select="#d1"></div>');
-        div.click();
-        this.server.respond();
-        div.innerHTML.should.equal("<div id=\"d1\">foo</div>");
-    });
+  it('properly handles a full HTML document', function() {
+    var i = 1
+    this.server.respondWith('GET', '/test', "<html><body><div id='d1'>foo</div><div id='d2'>bar</div></body></html>")
+    var div = make('<div hx-get="/test" hx-select="#d1"></div>')
+    div.click()
+    this.server.respond()
+    div.innerHTML.should.equal('<div id="d1">foo</div>')
+  })
 
-    it('properly handles a full HTML document  w/ data-* prefix', function()
-    {
-        var i = 1;
-        this.server.respondWith("GET", "/test", "<html><body><div id='d1'>foo</div><div id='d2'>bar</div></body></html>");
-        var div = make('<div hx-get="/test" data-hx-select="#d1"></div>');
-        div.click();
-        this.server.respond();
-        div.innerHTML.should.equal("<div id=\"d1\">foo</div>");
-    });
+  it('properly handles a full HTML document  w/ data-* prefix', function() {
+    var i = 1
+    this.server.respondWith('GET', '/test', "<html><body><div id='d1'>foo</div><div id='d2'>bar</div></body></html>")
+    var div = make('<div hx-get="/test" data-hx-select="#d1"></div>')
+    div.click()
+    this.server.respond()
+    div.innerHTML.should.equal('<div id="d1">foo</div>')
+  })
 })
