4ac25b6130998f39c1b0daeeab7d104fc0ab03fa
Neel Raja <neelrr1@gmail.com>
2024-01-10 08:59:08 +0000

    Emit htmx:sseBeforeMessage

--- src/ext/sse.js
+++ src/ext/sse.js
@@ -148,6 +148,7 @@ This extension adds support for Server Sent Events to htmx.  See /www/extensions
 					}
 
 					// swap the response into the DOM and trigger a notification
+					api.triggerEvent(elt, "htmx:sseBeforeMessage", event);
 					swap(child, event.data);
 					api.triggerEvent(elt, "htmx:sseMessage", event);
 				};
