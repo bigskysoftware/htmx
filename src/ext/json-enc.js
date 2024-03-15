0ca64cd30267edb990de6581aafba5e5ba441de9
Baraa Al-Masri <pub@mbaraa.com>
2024-03-15 17:57:00 +0300

    patch: fix numeric values parsed as string

--- src/ext/json-enc.js
+++ src/ext/json-enc.js
@@ -1,12 +1,18 @@
-htmx.defineExtension('json-enc', {
-    onEvent: function (name, evt) {
-        if (name === "htmx:configRequest") {
-            evt.detail.headers['Content-Type'] = "application/json";
-        }
-    },
-    
-    encodeParameters : function(xhr, parameters, elt) {
-        xhr.overrideMimeType('text/json');
-        return (JSON.stringify(parameters));
+htmx.defineExtension("json-enc", {
+  onEvent: function (name, evt) {
+    if (name === "htmx:configRequest") {
+      evt.detail.headers["Content-Type"] = "application/json";
     }
+  },
+
+  encodeParameters: function (xhr, parameters, elt) {
+    xhr.overrideMimeType("text/json");
+    for (const key in parameters) {
+      const tryNum = parseFloat(parameters[key]);
+      if (parameters[key] == tryNum) {
+        parameters[key] = tryNum;
+      }
+    }
+    return JSON.stringify(parameters);
+  },
 });
