adfd2c8bc8c710ee6935af7644de04e3081d099e
Carson Gross <carson@bigsky.software>
2024-04-17 10:58:31 -0600

    fix sha for unminified version

--- scripts/sha.sh
+++ scripts/sha.sh
@@ -1,2 +1,6 @@
+echo "htmx.min.js:"
 cat dist/htmx.min.js | openssl dgst -sha384 -binary | openssl base64 -A
+echo ""
+echo "htmx.js:"
+cat dist/htmx.js | openssl dgst -sha384 -binary | openssl base64 -A
 echo ""
