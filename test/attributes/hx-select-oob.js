129b07d935befbab0f6578bddad32fa0bde555b9
Abhinav Krishna <abhinavkrishnacr2020@gmail.com>
2024-08-08 23:23:57 +0530

    Fix test case for hx-select-oob

--- test/attributes/hx-select-oob.js
+++ test/attributes/hx-select-oob.js
@@ -74,13 +74,13 @@ describe("hx-select-oob attribute", function () {
   it('supports non-id selectors with colons', function()
   {
       this.server.respondWith('GET', '/test', "<div id='d1'>foo</div><div id='d2'>bar</div><div id='d3'>baz</div>")
-      var div = make('<div hx-get="/test" hx-select="#d1" hx-select-oob="#d2:has(~ #d3)"></div>')
+      var div = make('<div id="d4" hx-get="/test" hx-select="#d1" hx-select-oob="#d2:has(~ #d3):innerHTML"></div>')
       make('<div id="d2"></div>')
       div.click()
       this.server.respond()
-      div.innerHTML.should.equal('<div id="d1">foo</div>')
       var div2 = byId('d2')
       div2.innerHTML.should.equal('bar')
+      div.innerHTML.should.equal('<div id="d1">foo</div>')
   })
 });
 
