913bbe19c8e97ad2179f86d8d1cb5c06d262b28a
David Wilde <d.wilde@selligence.com>
2024-09-05 21:41:46 +0100

    Add view transitions on navigating history
    The view transitions are added when the user navigates back and forth
    through the history for configs that have enabled globalViewTransitions.

--- src/htmx.js
+++ src/htmx.js
@@ -3205,19 +3205,30 @@ var htmx = (function() {
     path = path || location.pathname + location.search
     const cached = getCachedHistory(path)
     if (cached) {
-      const fragment = makeFragment(cached.content)
-      const historyElement = getHistoryElement()
-      const settleInfo = makeSettleInfo(historyElement)
-      handleTitle(cached.title)
-      handlePreservedElements(fragment)
-      swapInnerHTML(historyElement, fragment, settleInfo)
-      restorePreservedElements()
-      settleImmediately(settleInfo.tasks)
-      getWindow().setTimeout(function() {
-        window.scrollTo(0, cached.scroll)
-      }, 0) // next 'tick', so browser has time to render layout
-      currentPathForHistory = path
-      triggerEvent(getDocument().body, 'htmx:historyRestore', { path, item: cached })
+      let restoreHistoryFromCache = function() {
+        const fragment = makeFragment(cached.content)
+        const historyElement = getHistoryElement()
+        const settleInfo = makeSettleInfo(historyElement)
+        handleTitle(cached.title)
+        handlePreservedElements(fragment)
+        swapInnerHTML(historyElement, fragment, settleInfo)
+        restorePreservedElements()
+        settleImmediately(settleInfo.tasks)
+        getWindow().setTimeout(function() {
+          window.scrollTo(0, cached.scroll)
+        }, 0) // next 'tick', so browser has time to render layout
+        currentPathForHistory = path
+        triggerEvent(getDocument().body, 'htmx:historyRestore', { path, item: cached })
+      }
+      const shouldTransition = htmx.config.globalViewTransitions;
+      if (shouldTransition && document.startViewTransition) {
+        const innerRestoreHistoryFromCache = restoreHistoryFromCache
+        // wrap the original doRestoreHistory() in a call to startViewTransition()
+        restoreHistoryFromCache = document.startViewTransition(
+          () => innerRestoreHistoryFromCache()
+        );
+      }
+      restoreHistoryFromCache()
     } else {
       if (htmx.config.refreshOnHistoryMiss) {
         // @ts-ignore: optional parameter in reload() function throws error
