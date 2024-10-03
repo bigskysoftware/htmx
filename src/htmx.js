4916ce4d0260072f3ceb2957d75229c88cf59fc2
Jackie Li <jackie.space@gmail.com>
2024-10-03 02:08:39 +0100

    fix #2932: check parent is null for swap delete (#2933)
    * fix #2932: check parent is null for swap
    
    * fix test in swap when parent elt deleted

--- src/htmx.js
+++ src/htmx.js
@@ -1663,9 +1663,13 @@ var htmx = (function() {
     /** @type {Node} */
     let newElt
     const eltBeforeNewContent = target.previousSibling
-    insertNodesBefore(parentElt(target), target, fragment, settleInfo)
+    const parentNode = parentElt(target)
+    if (!parentNode) { // when parent node disappears, we can't do anything
+      return
+    }
+    insertNodesBefore(parentNode, target, fragment, settleInfo)
     if (eltBeforeNewContent == null) {
-      newElt = parentElt(target).firstChild
+      newElt = parentNode.firstChild
     } else {
       newElt = eltBeforeNewContent.nextSibling
     }
@@ -1727,7 +1731,10 @@ var htmx = (function() {
    */
   function swapDelete(target) {
     cleanUpElement(target)
-    return parentElt(target).removeChild(target)
+    const parent = parentElt(target)
+    if (parent) {
+      return parent.removeChild(target)
+    }
   }
 
   /**
