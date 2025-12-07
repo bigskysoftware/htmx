3b21c641938e4b00ea69ae1e9888090905c8b4e7
MichaelWest22 <michael@latent.nz>
2025-12-08 05:00:47 +1300

    improve parseConfig and append to allow joining json for hx-vals (#3577)
    Co-authored-by: MichaelWest22 <michael.west@docuvera.com>

--- test/tests/attributes/hx-vals.js
+++ test/tests/attributes/hx-vals.js
@@ -8,6 +8,18 @@ describe('hx-vals attribute', function() {
         cleanupTest(this.currentTest)
     })
 
-    // TODO - convert to a direct test
+    it('hx-vals:append merges JSON objects correctly', async function() {
+        mockResponse('POST', '/test', 'Success')
+        const div = createProcessedHTML(
+            '<div hx-vals:inherited=\'{"a":1}\'>' +
+            '  <button hx-post="/test" hx-vals:append=\'{"b":"hi"}\'>Click</button>' +
+            '</div>'
+        );
+        const button = div.querySelector('button');
+        button.click();
+        await forRequest()
+        fetchMock.calls[0].request.body.get('a').should.equal('1');
+        fetchMock.calls[0].request.body.get('b').should.equal('hi');
+    });
 
 })
