925981b27692242a508ef525e022fb96a6e31e80
MichaelWest22 <michael.west@docuvera.com>
2025-12-12 01:23:24 +1300

    use firstchild check instead

--- src/htmx.js
+++ src/htmx.js
@@ -1088,7 +1088,7 @@ var htmx = (() => {
                 }
                 this.__trigger(elt, "htmx:after:cleanup")
             }
-            if (elt.nodeType === 1) {
+            if (elt.firstChild) {
                 for (let child of elt.querySelectorAll('[data-htmx-powered]')) {
                     this.__cleanup(child);
                 }
