07998a07a1d88431bab62c5a533bd79d46517b2e
Carson Gross <carson@bigsky.software>
2026-03-03 20:09:16 -0700

    properly escape meta-character for hx-status attributes

--- src/htmx.js
+++ src/htmx.js
@@ -2149,7 +2149,7 @@ var htmx = (() => {
                     ctx.swap = "none";
                     return
                 }
-                let statusValue = this.__attributeValue(ctx.sourceElement, "hx-status:" + pattern);
+                let statusValue = this.__attributeValue(ctx.sourceElement, this.__maybeAdjustMetaCharacter("hx-status:") + pattern);
                 if (statusValue) {
                     this.__mergeConfig(statusValue, ctx);
                     return;
