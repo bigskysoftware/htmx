c9ac333978f53affadbb21f9b2e93fbc9f298ef1
Carson Gross <carson@bigsky.software>
2026-05-12 11:22:47 -0600

    regen dist

--- dist/htmx.esm.js
+++ dist/htmx.esm.js
@@ -110,7 +110,7 @@ var htmx = (() => {
         }
 
         #initHtmxConfig() {
-            this.version = '4.0.0-beta3'
+            this.version = '4.0.0-beta4'
             this.config = {
                 logAll: false,
                 prefix: "data-hx-",
