0b9ae3ca7b7b250cc404cfbafab04c5fdcfdbc96
Christian Tanul <git@christiantanul.com>
2026-06-16 17:27:58 +0300

    add BreadcrumbList JSON-LD structured data

--- www/src/lib/jsonld.js
+++ www/src/lib/jsonld.js
@@ -7,6 +7,12 @@
  */
 
 /**
+ * @typedef {Object} BreadcrumbItem
+ * @property {string} label
+ * @property {string} [href]
+ */
+
+/**
  * @typedef {Object} JsonLdOpts
  * @property {string} title
  * @property {string} description
@@ -14,38 +20,57 @@
  * @property {URL} url
  * @property {string} siteUrl
  * @property {ArticleMeta} [article]
+ * @property {BreadcrumbItem[]} [breadcrumbs]
  */
 
 /**
  * Build the JSON-LD structured-data payload for a page.
- * Returns a WebSite schema by default; an Article schema when `article` is provided.
+ * Returns an array of schemas: WebSite or Article, plus BreadcrumbList when breadcrumbs are provided.
  * @param {JsonLdOpts} opts
  */
-export function buildJsonLd({ title, description, image, url, siteUrl, article }) {
+export function buildJsonLd({ title, description, image, url, siteUrl, article, breadcrumbs }) {
     const desc = description.trim();
+    const schemas = [];
+
     if (!article) {
-        return {
+        schemas.push({
             "@context": "https://schema.org",
             "@type": "WebSite",
             "name": "htmx",
             "url": siteUrl,
             "description": desc,
-        };
+        });
+    } else {
+        schemas.push({
+            "@context": "https://schema.org",
+            "@type": "Article",
+            "headline": title,
+            "description": desc,
+            "url": url.href,
+            "image": new URL(image, url).href,
+            ...(article.publishedTime && { "datePublished": article.publishedTime }),
+            ...(article.modifiedTime && { "dateModified": article.modifiedTime }),
+            ...(article.authors?.length && {
+                "author": article.authors.map(name => ({ "@type": "Person", name }))
+            }),
+            ...(article.tags?.length && { "keywords": article.tags.join(", ") }),
+            "publisher": { "@type": "Organization", "name": "htmx", "url": siteUrl },
+        });
     }
 
-    return {
-        "@context": "https://schema.org",
-        "@type": "Article",
-        "headline": title,
-        "description": desc,
-        "url": url.href,
-        "image": new URL(image, url).href,
-        ...(article.publishedTime && { "datePublished": article.publishedTime }),
-        ...(article.modifiedTime && { "dateModified": article.modifiedTime }),
-        ...(article.authors?.length && {
-            "author": article.authors.map(name => ({ "@type": "Person", name }))
-        }),
-        ...(article.tags?.length && { "keywords": article.tags.join(", ") }),
-        "publisher": { "@type": "Organization", "name": "htmx", "url": siteUrl },
-    };
+    if (breadcrumbs?.length) {
+        const allCrumbs = [{ label: 'Home', href: '/' }, ...breadcrumbs];
+        schemas.push({
+            "@context": "https://schema.org",
+            "@type": "BreadcrumbList",
+            "itemListElement": allCrumbs.map((crumb, i) => ({
+                "@type": "ListItem",
+                "position": i + 1,
+                "name": crumb.label.replace(/<[^>]*>/g, ''),
+                ...(crumb.href ? { "item": new URL(crumb.href, siteUrl).href } : {}),
+            })),
+        });
+    }
+
+    return schemas.length === 1 ? schemas[0] : schemas;
 }
