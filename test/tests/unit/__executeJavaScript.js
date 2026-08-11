2f5024ceea8f095f2f1c3b7025efe82e1451dd4b
Christian Tanul <git@christiantanul.com>
2026-08-11 22:28:49 +0300

    Let extensions rewrite JavaScript expressions

--- test/tests/unit/__executeJavaScript.js
+++ test/tests/unit/__executeJavaScript.js
@@ -13,4 +13,18 @@ describe('__executeJavaScript', function() {
         run().should.equal(42);
     });
 
+    it('lets extensions rewrite code before compilation', function() {
+        let extensions = backupExtensions();
+        clearExtensions();
+        htmx.registerExtension('rewrite-test', {
+            htmx_scope: (_, detail) => detail.code = 'value * 2'
+        });
+        try {
+            let elt = document.createElement('div');
+            htmx.__executeJavaScript(elt, { value: 21 }, 'value + 1', true, false).should.equal(42);
+        } finally {
+            restoreExtensions(extensions);
+        }
+    });
+
 });
